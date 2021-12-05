7a2b202932c175d37a24b73a4bdcd17f0829baf5
Dark-Roll <ai791016ai791016@gmail.com>
2021-12-05 19:00:04 +0800

    a lot of changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
+import react, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Amplify, { API } from 'aws-amplify';
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig);
+
+
 
 function App() {
+  useEffect(() => {
+    const apiName = 'myrest';
+    const path = '/items'; 
+    const myInit = { // OPTIONAL
+        headers: {}, // OPTIONAL
+        response: true, // OPTIONAL (return the entire Axios response object instead of only response.data)
+        queryStringParameters: {  // OPTIONAL
+            name: 'param',
+        },
+    };
+
+    API
+      .get(apiName, path, myInit)
+      .then(response => {
+        console.log('res is: ', response)
+        // Add your code here
+      })
+      .catch(error => {
+        console.log(error.response);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
